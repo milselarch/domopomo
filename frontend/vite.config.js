53d36f9687c14dd5ceaf9d63dff28522056efdf8
milselarch <milselarch@gmail.com>
2026-04-12 14:27:47 +0800

    fix: add vue plugin to process jsx

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import wails from "@wailsio/runtime/plugins/vite";
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wails("./bindings")],
+  plugins: [wails("./bindings"), vue()],
 });
